64173dae7135aa6f7baff99124d276c4269fbef1
Mynul Islam <mynul.sakil@gmail.com>
2023-05-03 15:07:22 +0600

    ad router for each id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 const app = express();
 const chefsJson = require('./data/chefs.json')
 // Use cors middleware to enable CORS
@@ -16,6 +16,11 @@ app.get('/chefs', (req, res) => {
 app.get('/greet/:name', (req, res) => {
     const { name } = req.params;
     res.send(`Hello, ${name}!`);
+});
+app.get('/chef/:id', (req, res) => {
+    const { id } = req.params;
+   
+    res.send(chef)
 });
 // Start the server on port 3000
 app.listen(PORT, () => {
